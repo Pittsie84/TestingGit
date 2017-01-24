f5f429e8c97729d6239b94cba7bf156c4f369904
Pittsie84 <pittss@chc.edu>
2017-01-24 08:52:00 -0500

    Version 1

---
+++ src/testinggit/TestingGit.java
@@ -0,0 +1,17 @@
+
+package testinggit;
+
+/**
+ *
+ * @author gabor_000
+ */
+public class TestingGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
